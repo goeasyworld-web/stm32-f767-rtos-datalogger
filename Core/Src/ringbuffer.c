8a780b4c35961822cc9882c41ed6474b67946850
goeasy <goeasyworld@gmail.com>
2026-07-13 23:56:56 +0530

    Day2: Ring buffer module added

---
+++ Core/Src/ringbuffer.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "ringbuffer.h"
+
+
+
+bool rb_is_full(ringbuf_t *rb)
+{
+	return (((rb->head +1) % RB_SIZE) == rb->tail);
+}
+
+
+bool rb_is_empty(ringbuf_t *rb)
+{
+	return rb->head == rb->tail;
+}
+
+bool rb_put(ringbuf_t *rb, uint8_t byte)
+{
+	if(rb_is_full(rb))
+	{
+		return false;
+	}
+	else
+	{
+		rb->bufferdata[rb->head] = byte;
+		rb->head = (rb->head +1) % RB_SIZE;
+		return true;
+	}
+}
+
+bool rb_get(ringbuf_t *rb, uint8_t *byte)
+{
+	if(rb_is_empty(rb))
+	{
+		return false;
+	}
+	else
+	{
+		*byte = rb->bufferdata[rb->tail];
+		rb->tail = (rb->tail + 1) % RB_SIZE;
+		return true;
+	}
+}
+
+void rb_init(ringbuf_t *rb)
+{
+	rb->head = 0;
+	rb->tail = 0;
+	for(uint8_t i = 0; i<RB_SIZE; i++)
+	{
+		rb->bufferdata[i] = 0;
+	}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Core/Src/ringbuffer.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+
+#ifndef	RINGBUFFER_H
+#define RINGBUFFER_H
+
+#define RB_SIZE			(64u)
+
+typedef struct ringbuffer{
+	uint8_t bufferdata[RB_SIZE];
+	volatile uint8_t head;
+	volatile uint8_t tail;
+}ringbuf_t;
+
+
+
+
+void rb_init(ringbuf_t	*rb);
+bool rb_put(ringbuf_t *rb, uint8_t byte);
+bool rb_get(ringbuf_t *rb, uint8_t *byte);
+bool rb_is_empty(ringbuf_t*);
+bool rb_is_full(ringbuf_t*);
+
+#endif
+
+
+
